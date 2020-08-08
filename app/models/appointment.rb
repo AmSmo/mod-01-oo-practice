d0a4ebf362d93e5ca18ee9ab14f63eb82e9b09ba
AmSmo <adam.smolenski@gmail.com>
2020-08-08 11:07:34 -0400

    part 4

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -14,6 +14,10 @@ class Appointment
         @@all << self
     end
 
+    # will add a time and reference that later
+    # hesitant to do things like that without knowing
+    # how far we are going with deliverables
+    # but HELLO! hope you're having a wonderful day
 
 
 end
